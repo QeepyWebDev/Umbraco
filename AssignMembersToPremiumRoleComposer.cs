4db858966395285392b7d6d0f2d1e3d345ce0483
QeepyWebDev <145613539+QeepyWebDev@users.noreply.github.com>
2024-09-05 16:15:13 +0200

    Add project files.

---
+++ AssignMembersToPremiumRoleComposer.cs
@@ -0,0 +1,12 @@
+﻿using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.Notifications;
+
+namespace NewUmbracoProject
+{
+    public class AssignMembersToPremiumRoleComposer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        => builder.AddNotificationHandler<MemberSavedNotification, AssignMembersToPremiumRoleHandler>();
+    }
+}
+
